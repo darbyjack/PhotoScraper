b835070c6648c91d17a694cd48c4ab57da4bee0d
darbyjack <admin@glaremasters.me>
2018-10-25 13:53:36 -0500

    Create a folder with the root url of what's being used

--- src/main/java/PhotoScraper.java
+++ src/main/java/PhotoScraper.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+
 /**
  * Created by GlareMasters
  * Date: 10/25/2018
@@ -17,10 +19,18 @@ public class PhotoScraper {
         String url = args[0];
         String memberCount = args[1];
 
+        int slashslash = url.indexOf("//") + 2;
+
+        String rootDomain = url.substring(slashslash, url.indexOf('/', slashslash));
+
+        File dir = new File(rootDomain);
+        dir.mkdir();
+
         WebDriver driver = new ChromeDriver(options);
-        driver.get(url.replace("{}", String.valueOf(1)));
-        System.out.println(url);
-        System.out.println(memberCount);
+        for (int i = 1; i < Integer.valueOf(memberCount); i++) {
+            driver.get(url.replace("{}", String.valueOf(i)));
+        }
+
         driver.close();
     }
 
